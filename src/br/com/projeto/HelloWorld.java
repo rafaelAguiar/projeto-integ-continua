2f0f9ea0b6f30c72069d98b82bdd2c2a752e9175
Rafael Aguiar <rafadeaguiar@gmail.com>
2014-08-25 12:28:05 -0300

    Update HelloWorld.java

--- src/br/com/projeto/HelloWorld.java
+++ src/br/com/projeto/HelloWorld.java
@@ -26,7 +26,7 @@ public class HelloWorld extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().println("Hello World Man!!!");
+		response.getWriter().println("Hello World Man!!!!");
 	}
 
 	/**
